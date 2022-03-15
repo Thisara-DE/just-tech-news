7fe305caff500890cc3fd917ca884ce4f6efe8d1
Glorzo <thisaraeranga@gmail.com>
2022-03-14 23:14:15 -0500

    added Post model and initialized it

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const sequelize = require('../config/connection');
 class User extends Model {
     // setup method to run on instance data (per user) to check password
     checkPassword(loginPW) {
-        return bcrypt.compareSync(loginPW, this.password);
+        return bcrypt.compareSync(loginPW, this.password); 
     }
 }
 
