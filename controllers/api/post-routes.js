f564dc7af0ddb7a4ae643d33c26d810f9c2a1a36
Glorzo <thisaraeranga@gmail.com>
2022-03-23 08:32:04 -0500

    add upvoting frontend js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -76,7 +76,7 @@ router.post('/', (req,res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req,res) => {
-    // custom static method created in models/Post.js
+    // custom static method created in models/Post.js    
     Post.upvote(req.body, { Vote })
     .then(updatedPostData => res.json(updatedPostData))
     .catch(err => {
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,27 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+            email,
+            password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        } 
+    }
+}
+
 
 async function signupFormHandler(event) {
     event.preventDefault();
@@ -26,29 +50,29 @@ async function signupFormHandler(event) {
     }
 }
 
-async function loginFormHandler(event) {
-    event.preventDefault();
+// async function loginFormHandler(event) {
+//     event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+//     const email = document.querySelector('#email-login').value.trim();
+//     const password = document.querySelector('#password-login').value.trim();
 
-    if(email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+//     if(email && password) {
+//         const response = await fetch('/api/users/login', {
+//             method: 'post',
+//             body: JSON.stringify({
+//                 email,
+//                 password
+//             }),
+//             headers: { 'Content-Type': 'application/json' }
+//         });
 
-        if(response.ok) {
-            document.location.replace('/');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
+//         if(response.ok) {
+//             document.location.replace('/');
+//         } else {
+//             alert(response.statusText);
+//         }
+//     }
+// }
 
 
 
