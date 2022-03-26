768a68251bec064df9bc8f72af8834f490bb4232
Glorzo <thisaraeranga@gmail.com>
2022-03-26 14:34:42 -0500

    secured the sess

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
-    secret: 'Super secret secret',
+    secret: process.env.SESSION_KEY,
     cookie: {},
     resave: false,
     saveUninitialized: true,
